4754ee32f0abca3c7dd16d4527763473731e5eea
Nicolas Bravo <nicojjj4@gmail.com>
2024-10-10 10:53:59 -0300

    Se creo una nueva vista para poder enviar tokens y agregar nuevas categorias

--- SGIStockMaster/inventory_manager/urls.py
+++ SGIStockMaster/inventory_manager/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('agregar-producto/', views.agregar_producto, name='agregar_producto'),  
+    path('agregar-producto/', views.agregar_producto, name='agregar_producto'),
+    path('crear-categoria/', views.crear_categoria, name='crear_categoria'),
 ]
--- SGIStockMaster/inventory_manager/views.py
+++ SGIStockMaster/inventory_manager/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def agregar_producto(request):
     return render(request, 'inventory_manager/SGI_agregar_producto.html')
+
+def crear_categoria(request):
+    return render(request, 'inventory_manager/SGI_crear_categoria.html')
