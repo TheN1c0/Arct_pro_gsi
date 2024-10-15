9dc765d22879465ddabe3d51792e524ccf831997
El UN1c0 <133818721+TheN1c0@users.noreply.github.com>
2024-10-14 22:58:31 -0300

    Se agregó la nueva vista para editar categorías, falta revisión. Además, es importante documentar cada parte

--- SGIStockMaster/inventory_manager/urls.py
+++ SGIStockMaster/inventory_manager/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('agregar-producto/', views.agregar_producto, name='agregar_producto'),  
+    path('administracion_categorias/', views.administracion_categorias, name='administracion_categorias'),  
+    
 ]
--- SGIStockMaster/inventory_manager/views.py
+++ SGIStockMaster/inventory_manager/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-
 def agregar_producto(request):
     return render(request, 'inventory_manager/SGI_agregar_producto.html')
+
+def administracion_categorias(request):
+    return render(request, 'inventory_manager/SGI_administracion_categorias.html')
